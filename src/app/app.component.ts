80889aab3f406732fa35da950f6440c00926565d
Rosa M. Burrueco <rosam.burrueco@juntadeandalucia.es>
2022-03-13 13:12:25 +0100

    Prueba Angular app contador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+  titulo: string = 'Contador app';
+  numero: number = 10;
+
+  acumular( valor: number ) {
+    this.numero += valor; 
+  }
 }
