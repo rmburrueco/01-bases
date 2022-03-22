ffb027891a15bbf602e33ab3292a304872414e90
Rosa M. Burrueco <rosam.burrueco@juntadeandalucia.es>
2022-03-22 12:43:11 +0100

    Uso de servicios

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Personaje } from "../interfaces/dbz.interface";
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-main-page',
@@ -7,26 +8,15 @@ import { Personaje } from "../interfaces/dbz.interface";
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent {
-
-  personajes: Personaje[] = [
-    {
-      nombre: 'Goku',
-      poder: 15000
-    },
-    {
-      nombre: 'Vegeta',
-      poder: 7500
-    }
-
-  ];
-
+  
   nuevo: Personaje = {
-    nombre: 'Mario',
-    poder: 50000
+    nombre: 'Maestro Roshi',
+    poder: 1000
   }
 
-  agregarNuevoPersonaje( argumento: Personaje){
-    this.personajes.push( argumento );
-  }
+  // get personajes():Personaje[] {
+  //   return this.dbzService.personajes;
+  // } 
 
+  constructor( private dbzService: DbzService ){}
 }
